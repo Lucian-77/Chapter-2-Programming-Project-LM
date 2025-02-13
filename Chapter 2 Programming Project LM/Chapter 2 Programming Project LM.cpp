a9f62491a81767cda0b3feaf460de321e6844fe4
Lucian-77 <lucianshambhu@gmail.com>
2025-02-13 13:31:58 -0500

    erased old code

--- Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
+++ Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
@@ -3,17 +3,7 @@
 
 #include <iostream>
 
-int main()
+int ED()
 {
-    std::cout << "How much higher will the sea level be in x years?\n";
-    double rOChange = 1.5;
-    int five = 5;
-    double fydOceanLevel = five * rOChange;
-    std::cout << "5: " << fydOceanLevel << "mm higher\n";
-    int seven = 7;
-    double sydOceanLevel = seven * rOChange;
-    std::cout << "7: " << sydOceanLevel << "mm higher\n";
-    int ten = 10;
-    double tydOceanLevel = ten * rOChange;
-    std::cout << "10: " << tydOceanLevel << "mm higher\n";
+ 
 }
