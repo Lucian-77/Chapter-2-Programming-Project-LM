64487986881f8c9915dd8b737c3865ee46f42394
Lucian-77 <lucianshambhu@gmail.com>
2025-02-13 13:31:23 -0500

    erased old code

--- Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
+++ Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
@@ -1,19 +1,9 @@
-// Chapter 2 Programming Project LM.cpp : This file contains the 'Ocean Level' function.
+// Chapter 2 Programming Project LM.cpp : This file contains the 'Tank of Gas' function.
 //
 
 #include <iostream>
 
-int main()
+int TOG()
 {
-    std::cout << "How much higher will the sea level be in x years?\n";
-    double rOChange = 1.5;
-    int five = 5;
-    double fydOceanLevel = five * rOChange;
-    std::cout << "5: " << fydOceanLevel << "mm higher\n";
-    int seven = 7;
-    double sydOceanLevel = seven * rOChange;
-    std::cout << "7: " << sydOceanLevel << "mm higher\n";
-    int ten = 10;
-    double tydOceanLevel = ten * rOChange;
-    std::cout << "10: " << tydOceanLevel << "mm higher\n";
+   
 }
