fa37888702b8bc6f798b67f2e1cf0f430082a81e
Lucian-77 <lucianshambhu@gmail.com>
2025-02-14 13:13:00 -0500

    Built the code

--- Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
+++ Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
@@ -1,9 +1,17 @@
-// Chapter 2 Programming Project LM.cpp : This file contains the 'Ocean Level' function.
+// Chapter 2 Programming Project LM.cpp : This file contains the 'Energy Drink' function.
 //
 
 #include <iostream>
 
-int ED()
+int main()
 {
- 
+	std::cout << "How many people in our survey bought multiple drinks per week?\n";
+	int tPeople = 16500;
+	double pOF = 0.15;
+	int mDPW = tPeople * pOF;
+	std::cout << "About 15% or " << mDPW << " people\n" << "\n";
+	std::cout << "How many of those people prefered citrus flavors?\n";
+	double pFE = 0.58;
+	int pCF = mDPW * pFE;
+	std::cout << "About 58% or " << pCF << " people\n";
 }
