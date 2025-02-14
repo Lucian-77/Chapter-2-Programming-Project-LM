b0ce307f2cf7c2e51469e87eaed204dabe4bf5d1
Lucian-77 <lucianshambhu@gmail.com>
2025-02-14 12:43:22 -0500

    Built the code

--- Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
+++ Chapter 2 Programming Project LM/Chapter 2 Programming Project LM.cpp
@@ -3,7 +3,14 @@
 
 #include <iostream>
 
-int TOG()
+int main()
 {
-   
+	std::cout << "How many miles can I go with a full tank of gas in town vs on the highway?\n";
+	int gTank = 20;
+	double dITown = 23.5;
+	double dTown = gTank * dITown;
+	std::cout << "In town: " << dTown << " miles\n";
+	double dOHigh = 28.9;
+	double dHigh = gTank * dOHigh;
+	std::cout << "On the highway: " << dHigh << " miles\n";
 }
